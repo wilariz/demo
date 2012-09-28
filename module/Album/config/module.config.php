3d2709867d131a49f5681eb1a315e369071639a0
Wilmer Arteaga <wilarizz@hotmail.com>
2012-09-27 20:40:49 -0500

    subiendo proyecto

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,65 @@
+<?php
+namespace Album;
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Index' => 'Album\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    // Change this to something specific to your module
+                    'route'    => '/album',
+                    'defaults' => array(
+                        // Change this value to reflect the namespace in which
+                        // the controllers for your module are found
+                        '__NAMESPACE__' => 'Album\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    // This route is a sane default when developing a module;
+                    // as you solidify the routes for your module, however,
+                    // you may want to remove it and replace it with more
+                    // specific routes.
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Album' => __DIR__ . '/../view',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'),
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Album\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        ),
+    ),
+);
---
+++ module/Album/src/Album/Entity/Album.php
@@ -0,0 +1,57 @@
+<?php
+namespace Album\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="album")
+ */
+class Album
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $artista;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $titulos;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fecha;
+
+
+    /**
+     * [getId() description here]
+     *
+     * @return [type] [description]
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * [setId() description here]
+     *
+     * @param  [type] $id [description]
+     * @return [class type]    $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
